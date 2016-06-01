df71a9bb1edebaf58e64460a1e139139033b155e
Nigel Rahkola <me@nigelrahkola.com>
2016-06-01 08:32:39 -0700

    Sketch a main function that takes intput

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf( "Available operations are +, -, *, /" );
+    scanf( "%d", &x );
+    printf( "You entered %d", x );
+    getchar();
+    return 0;
+}
