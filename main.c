46b09df753793cfb755bb8e69ef95bc4286f730f
Nigel Rahkola <me@nigelrahkola.com>
2016-06-02 07:21:50 -0700

    Find the length of the expression
    Let's use this for looping over the input.

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-    int x;
-    printf( "Available operations are +, -, *, /" );
-    scanf( "%d", &x );
-    printf( "You entered %d", x );
+int calculate(char*);
+
+int main() {
+    char expression[4] = "";
+    printf("Available operations are +, -, *, /. End your expression with a $ character\n");
+    scanf("%3s", expression);
+    printf("%s", expression);
+    printf("len of expression: %lu\n", strlen(expression));
+    calculate(expression);
+    printf("\n");
     getchar();
+    return 0;
+}
+
+int calculate(char* expression) {
+    printf("%s", expression);
+    char c = (char) expression;
+    while(strcmp(&c, "\0")) {
+        printf("%c", c);
+        c++;
+    }
     return 0;
 }
--- makefile
+++ makefile
@@ -2,3 +2,6 @@ main : main.c
 	gcc main.c -o main
 clean:
 	rm main
+run : main.c
+	gcc main.c -o main
+	./main
