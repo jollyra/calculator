3ceb4e0518d25d79bf6195453e5eed17294e66ab
Nigel Rahkola <me@nigelrahkola.com>
2016-06-07 13:55:10 -0700

    Implement a bare-bones scanner

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define TRUE  1
@@ -17,14 +18,19 @@ struct Token
     char* attribute_value;
 };
 
-char** scanner(char*);
-struct Token** parser(struct Token* symbol_table);
+struct Token** scanner(struct Token** symbol_table, int*, char*);
+/* struct Token** parser(struct Token* symbol_table); */
 
 int main() {
     char expression[16] = "";
     printf("Available operations are +, -, *, /\n");
     scanf("%15s", expression);
-    scanner(expression);
+
+    // symbol table
+    struct Token* symbol_table[16];
+    int* size = malloc(sizeof(int));
+    scanner(symbol_table, size, expression);
+
     printf("\n");
     getchar();
     return 0;
@@ -33,14 +39,34 @@ int main() {
 // Lexical Analysis
 // take the string of input characters and group them into meaningful chunks
 // called 'lexemes'.
-char** scanner(char* expression) {
-    // symbol table
-    struct Token* symbol_table[16];
-
+struct Token** scanner(struct Token** symbol_table, int* size, char* expression) {
     for(int c = 0; c < sizeof(expression); c++) {
         printf("%c", expression[c]);
         if(expression[c] == '+') {
+            struct Token* t = malloc(sizeof(struct Token));
+            char* token_name = "+";
+            char* attribute_value = "+";
+            t->token_name = token_name;
+            t->attribute_value = attribute_value;
+            symbol_table[*size] = t;
+            (*size)+=1;
+        } else if(expression[c] == '-') {
+            struct Token* t = malloc(sizeof(struct Token));
+            char* token_name = "-";
+            char* attribute_value = "-";
+            t->token_name = token_name;
+            t->attribute_value = attribute_value;
+            symbol_table[*size] = t;
+            (*size)+=1;
+        } else if(expression[c] == '1') {
+            struct Token* t = malloc(sizeof(struct Token));
+            char* token_name = "NUM";
+            char* attribute_value = "1";
+            t->token_name = token_name;
+            t->attribute_value = attribute_value;
+            symbol_table[*size] = t;
+            (*size)+=1;
         }
     }
-    return 0;
+    return symbol_table;
 }
