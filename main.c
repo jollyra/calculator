e6d8dc6cbf6723692ce651f74edc5ad46384d935
Nigel Rahkola <me@nigelrahkola.com>
2016-06-07 16:25:54 -0700

    Use a separate struct for the symbol table

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #define TRUE  1
 #define FALSE 0
+#define SYMBOL_TABLE_SIZE 16
 
 /* Algorithm:
  * get the expression string
@@ -18,7 +19,14 @@ struct Token
     char* attribute_value;
 };
 
-struct Token** scanner(struct Token** symbol_table, int*, char*);
+struct SymbolTable
+{
+    struct Token* tokens[SYMBOL_TABLE_SIZE];
+    int count;
+};
+
+struct SymbolTable* scanner(char* expression);
+void print_symbol_table(struct SymbolTable* symbol_table);
 /* struct Token** parser(struct Token* symbol_table); */
 
 int main() {
@@ -26,10 +34,9 @@ int main() {
     printf("Available operations are +, -, *, /\n");
     scanf("%15s", expression);
 
-    // symbol table
-    struct Token* symbol_table[16];
-    int* size = malloc(sizeof(int));
-    scanner(symbol_table, size, expression);
+    // creat the symbol table
+    struct SymbolTable* symbol_table = scanner(expression);
+    print_symbol_table(symbol_table);
 
     printf("\n");
     getchar();
@@ -39,34 +46,46 @@ int main() {
 // Lexical Analysis
 // take the string of input characters and group them into meaningful chunks
 // called 'lexemes'.
-struct Token** scanner(struct Token** symbol_table, int* size, char* expression) {
+struct SymbolTable* scanner(char* expression) {
+    struct SymbolTable* symbol_table = malloc(sizeof(struct SymbolTable));
+
+    // scan through the expression and create tokens
     for(int c = 0; c < sizeof(expression); c++) {
-        printf("%c", expression[c]);
         if(expression[c] == '+') {
             struct Token* t = malloc(sizeof(struct Token));
             char* token_name = "+";
             char* attribute_value = "+";
             t->token_name = token_name;
             t->attribute_value = attribute_value;
-            symbol_table[*size] = t;
-            (*size)+=1;
+            symbol_table->tokens[symbol_table->count] = t;
+            symbol_table->count+=1;
         } else if(expression[c] == '-') {
             struct Token* t = malloc(sizeof(struct Token));
             char* token_name = "-";
             char* attribute_value = "-";
             t->token_name = token_name;
             t->attribute_value = attribute_value;
-            symbol_table[*size] = t;
-            (*size)+=1;
+            symbol_table->tokens[symbol_table->count] = t;
+            symbol_table->count+=1;
         } else if(expression[c] == '1') {
             struct Token* t = malloc(sizeof(struct Token));
             char* token_name = "NUM";
             char* attribute_value = "1";
             t->token_name = token_name;
             t->attribute_value = attribute_value;
-            symbol_table[*size] = t;
-            (*size)+=1;
+            symbol_table->tokens[symbol_table->count] = t;
+            symbol_table->count+=1;
         }
     }
     return symbol_table;
+}
+
+void print_symbol_table(struct SymbolTable* symbol_table)
+{
+    printf("symbol_table\n");
+    for(int i = 0; i < symbol_table->count; i++)
+    {
+        struct Token* symbol = symbol_table->tokens[i];
+        printf("\t<%s, %s>\n", symbol->token_name, symbol->attribute_value);
+    }
 }
