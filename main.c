1e5bbad298780051016630681182513156191d35
Nigel Rahkola <me@nigelrahkola.com>
2016-06-22 15:45:03 -0700

    Use abstract symbols to represent operations

--- main.c
+++ main.c
@@ -25,27 +25,36 @@
  */
 
 
-char** scan(char*);
-struct Node* parse(char** expression);
+char** read_input(char*);
+int* scan(char**);
+struct Node* parse(int* lexemes);
 int evaluate(struct Node*);
 int sum(int, int);
 int diff(int, int);
 int multiply(int, int);
 int divide(int, int);
 struct Node {
-    char* value;
+    int value;
     struct Node* lchild;
     struct Node* rchild;
 };
 int list_length(char**);
-int list_index(char** list, char* target);
-int get_highest_precedence_op_idx(char** expression);
-char** sublist(char**, int start, int end);
+int lexemes_len(int* lexemes);
+int list_index(int* list, int target);
+int get_highest_precedence_op_idx(int* lexemes);
+int* sublist(int*, int start, int end);
+
+int PLUS = 100;
+int MINUS = 101;
+int MUL = 102;
+int DIVIDE = 103;
+int ONE = 1;
 
 // Utility functions
 void print_btree(struct Node*);
 void print_str(char*);
 void print_tokens(char**);
+void print_lexemes(int*);
 char* maybeRemoveNewline(char*);
 
 void print_str(char* str) {
@@ -55,7 +64,7 @@ void print_str(char* str) {
 void print_btree(struct Node* root) {
     assert(root != NULL);
     printf("node: %p\t", root);
-    printf("value: %s\t", root->value);
+    printf("value: %d\t", root->value);
     printf("lchild: %p \trchild: %p\n", root->lchild, root->rchild);
     if(root->lchild) {
         print_btree(root->lchild);
@@ -74,10 +83,10 @@ int list_length(char** list) {
     return count;
 }
 
-int list_index(char** list, char* target) {
+int list_index(int* list, int target) {
     int idx = 0;
-    while(*list != NULL) {
-        if(strcmp(*list, target) == 0) {
+    while(*list != 0) {
+        if(*list == target) {
             return idx;
         }
         list++;
@@ -86,9 +95,18 @@ int list_index(char** list, char* target) {
     return -1;
 }
 
-char** sublist(char** list, int start, int end) {
+int lexemes_len(int* lexemes) {
+    int count = 0;
+    while(*lexemes != 0) {
+        count++;
+        lexemes++;
+    }
+    return count;
+}
+
+int* sublist(int* list, int start, int end) {
     // The last pointer must always be null
-    char** sublist = calloc(end - start + 2, sizeof(void*));
+    int* sublist = calloc(end - start + 2, sizeof(void*));
     int count = 0;
     int pos = start;
     while(pos <= end) {
@@ -99,11 +117,11 @@ char** sublist(char** list, int start, int end) {
     return sublist;
 }
 
-int get_highest_precedence_op_idx(char** expression) {
-    const char order_of_ops[] = "+-*/";
-    for(int i = 0; i < strlen(order_of_ops); i++) {
-        char op = order_of_ops[i];
-        int op_idx = list_index(expression, &op);
+int get_highest_precedence_op_idx(int* lexemes) {
+    int order_of_ops[] = {PLUS, MINUS, MUL, DIVIDE};
+    for(int i = 0; i < 4; i++) {  // TODO: Use a general list iteration with a unique terminating sentinal
+        int op = order_of_ops[i];
+        int op_idx = list_index(lexemes, op);
         if(op_idx >= 0) {
             return op_idx;
         }
@@ -113,25 +131,25 @@ int get_highest_precedence_op_idx(char** expression) {
 
 // Takes a simple mathematical expression and constructs a syntax tree
 // representation of it.
-struct Node* parse(char** expression) {
+struct Node* parse(int* lexemes) {
+    int len = lexemes_len(lexemes);
     struct Node* node = malloc(sizeof(struct Node));
     node->lchild = NULL;
     node->rchild = NULL;
 
-    int len = list_length(expression);
     if(len == 1) {
-        node->value = expression[0];
+        node->value = lexemes[0];
     } else if(len > 1) {
-        int op_idx = get_highest_precedence_op_idx(expression);
-        printf("highest precedence op is %s\n", expression[op_idx]);
+        int op_idx = get_highest_precedence_op_idx(lexemes);
+        printf("highest precedence op is %d\n", lexemes[op_idx]);
         if(op_idx >= 0) {
-            node->value = expression[op_idx];
+            node->value = lexemes[op_idx];
         }
 
-        char** lexpr = sublist(expression, 0, op_idx - 1);
+        int* lexpr = sublist(lexemes, 0, op_idx - 1);
         node->lchild = parse(lexpr);
 
-        char** rexpr = sublist(expression, op_idx + 1, len - 1);
+        int* rexpr = sublist(lexemes, op_idx + 1, len - 1);
         node->rchild = parse(rexpr);
     }
     return node;
@@ -160,19 +178,19 @@ int divide(int x, int y) {
 // Takes a syntax tree of a simple expression and evaluates it.
 int evaluate(struct Node* parent) {
     assert(parent != NULL);
-    char* val = parent->value;
-    if(strcmp(val, "+") == 0) {
+    int val = parent->value;
+    if(val == PLUS) {
         return sum(evaluate(parent->lchild), evaluate(parent->rchild));
-    } else if(strcmp(val, "-") == 0) {
+    } else if(val == MINUS) {
         return diff(evaluate(parent->lchild), evaluate(parent->rchild));
-    } else if(strcmp(val, "*") == 0) {
+    } else if(val == MUL) {
         return multiply(evaluate(parent->lchild), evaluate(parent->rchild));
-    } else if(strcmp(val, "/") == 0) {
+    } else if(val == DIVIDE) {
         return divide(evaluate(parent->lchild), evaluate(parent->rchild));
-    } else if(strcmp(val, "1") == 0) {
+    } else if(val == ONE) {
         return 1;
     } else {
-        printf("ERROR: unknown symbol \'%s\'\n", val);
+        printf("ERROR: unknown symbol \'%d\'\n", val);
         exit(EXIT_FAILURE);
     }
 }
@@ -193,8 +211,17 @@ void print_tokens(char** tokens) {
     }
 }
 
-char** scan(char* expression) {
-    char** tokens = calloc(MAX_EXPRESSION_SIZE*2-1, sizeof(void*));
+void print_lexemes(int* lexemes) {
+    int i = 0;
+    printf("lexemes:\n");
+    while(*lexemes != 0) {
+        printf("%d\n", *lexemes);
+        lexemes++;
+    }
+}
+
+char** read_input(char* expression) {
+    char** tokens = calloc(MAX_EXPRESSION_SIZE*2, sizeof(void*));
     const char* delim = " ";
     char* token;
     char** pos = tokens;
@@ -205,6 +232,24 @@ char** scan(char* expression) {
     return tokens;
 }
 
+int* scan(char** tokens) {
+    int num_tokens = list_length(tokens);
+    int* lexemes = calloc(num_tokens + 1, sizeof(int));
+    lexemes[num_tokens + 1] = 0;  // Unique terminating signal
+    for(int i = 0; i < num_tokens; i++) {
+        if(strcmp(tokens[i], "+") == 0) {
+            lexemes[i] = PLUS;
+        } else if(strcmp(tokens[i], "-") == 0) {
+            lexemes[i] = MINUS;
+        } else if(strcmp(tokens[i], "1") == 0) {
+            lexemes[i] = ONE;
+        } else {
+
+        }
+    }
+    return lexemes;
+}
+
 int main(int argc, char *argv[]) {
     printf("Available operations are +, -, *, /\n");
 
@@ -213,11 +258,16 @@ int main(int argc, char *argv[]) {
     char* expression = maybeRemoveNewline(input_buffer);
 
     char** tokens;
-    tokens = scan(expression);
+    tokens = read_input(expression);
+    int len = list_length(tokens);
     print_tokens(tokens);
 
+    int* lexemes;
+    lexemes = scan(tokens);
+    print_lexemes(lexemes);
+
     struct Node* syntax_tree;
-    syntax_tree = parse(tokens);
+    syntax_tree = parse(lexemes);
     print_btree(syntax_tree);
 
     int ans = evaluate(syntax_tree);
